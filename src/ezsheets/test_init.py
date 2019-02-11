c45e321b41afb600ce9a0d6f014377f7d38a3e15
Matthew Wishoff <mwishoff@gmail.com>
2019-02-10 16:10:40 -0800

    Added 5 test cases for getColumnNumber.

--- src/ezsheets/test_init.py
+++ src/ezsheets/test_init.py
@@ -3,47 +3,82 @@ import __init__ as init # for helper functions
 from __init__ import Spreadsheet
 from __init__ import Sheet
 
-class test_thing(unittest.TestCase):
+class test_getIdFromUrl(unittest.TestCase):
 
 	# Tests should change when behavior for bad urls passed in is decided.
 	# If bad urls are allowed the 404 should be caught in refresh(),
 	# Else more filtering should be done to URL input.
 
 	# Standard use case
-	def test_getIdFromUrl(self):
+	def test_IdFromUrl(self):
 		self.assertEqual(init.getIdFromUrl(r"https://docs.google.com/spreadsheets/d/10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng/edit#gid=0"), r"10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng")
 		
 		
 	# URL with a space in it after https://docs.google.com/spreadsheets/d/
-	def test_getIdFromUrl1(self):
+	def test_IdFromUrl1(self):
 		self.assertEqual(init.getIdFromUrl(r"https://docs.google.com/spreadsheets/d/10tRbpHZYkfRecHyRHR jBLdQYoq5QWNBqZmH9tt4Tjng/edit#gid=0"), r"10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng")
 		
 	
 	# URL with a space in it before the id https://docs.google.com/spreadsheets/d/
-	def test_getIdFromUrl2(self):
+	def test_IdFromUrl2(self):
 		self.assertEqual(init.getIdFromUrl(r"https://docs.google.com/spread sheets/d/10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng/edit#gid=0"), r"10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng")
 		
 		
 	# Invalid spreadsheet URL
-	def test_getIdFromUrl3(self):
+	def test_IdFromUrl3(self):
 		self.assertEqual(init.getIdFromUrl(r"https://google.com"), r"bad url")
 		
 		
-class test_get_column_letter_of(unittest.TestCase):
+class test_getColumnLetterOf(unittest.TestCase):
 
+	# Test from documentation in the code.
 	def test_ColumnLetterOf(self):
 		self.assertEqual(init.getColumnLetterOf(1), 'A')
 		
+	# Test from documentation in the code.
 	def test_ColumnLetterOf1(self):
 		self.assertEqual(init.getColumnLetterOf(27), 'AA')
-		
+	
+	# Test when the column doesn't exist (Negative number).
 	def test_ColumnLetterOf2(self):
 		self.assertEqual(init.getColumnLetterOf(-1), '')
 		
+	# Test for accuracy when dealing with a large number of columns.
 	def test_ColumnLetterOf3(self):
-		self.assertEqual(init.getColumnLetterOf(27 * 26), 'ZZ')
+		self.assertEqual(init.getColumnLetterOf(702), 'ZZ')
+	
+	# If a letter is given as input, the letter should probably just be returned.
+	def test_ColumnLetterOf4(self):
+		self.assertEqual(init.getColumnLetterOf('A'), 'A')
+		
+		
+class test_getColumnNumber(unittest.TestCase):
+	
+	def test_ColumnNumber(self):
+		self.assertEqual(init.getColumnNumber('A'), 1)
+		
+	def test_ColumnNumber1(self):
+		self.assertEqual(init.getColumnNumber(''), 0)
+		
+	def test_ColumnNumber2(self):
+		self.assertEqual(init.getColumnNumber('ZZ'), 702)
+		
+	def test_ColumnNumber3(self):
+		self.assertEqual(init.getColumnNumber(1), 1)
 	
 if __name__ == '__main__':
 	#init.Spreadsheet(r"https://docs.google.com/spreadsheets/d/10tRbpHZYkfRecHyRHRjBLdQYoq5QWNBqZmH9tt4Tjng/edit#gid=0")
-	print(init.getColumnLetterOf('A'))
-	# unittest.main()
+	# print(init.getColumnLetterOf('A'))
+	unittest.main()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
